7a9d093ce9ee915a4043e8df8337fa4fc72f7537
odalebrown <odale_brown@yahoo.com>
2020-04-10 02:59:09 -0400

    Updating code

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-# SECRET_KEY is needed for session security, the flash() method in this case stores the message in a session
+# SECRET_KEY is needed for session security, the flash() method in this case stores the messages in a session
 SECRET_KEY = 'Sup3r$3cretkey'
 
 app = Flask(__name__)
