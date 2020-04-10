75d50b7ce7560e5bdd4f7261f11cf02a94bcd897
odalebrown <odale_brown@yahoo.com>
2020-04-10 03:27:00 -0400

    Latest commit

--- app/views.py
+++ app/views.py
@@ -24,7 +24,7 @@ def home():
 
 @app.route('/about/')
 def about():
-    """Render the website's about page."""
+    """Render the website about page."""
     return render_template('about.html', name="Mary Jane")
 
 
